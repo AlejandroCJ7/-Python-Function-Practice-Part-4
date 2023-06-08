3499d71aa51d7eec5d7c86e598114b637bdabaf4
Alex Castaneda <alex@Alejandros-Mac.local>
2023-06-08 14:02:23 -0700

    wrote rev_string function

--- PythonFunction_Part4.py
+++ PythonFunction_Part4.py
@@ -11,9 +11,12 @@ def multi_list(a, b, c, d):
 print (multi_list(2, 7, 8, 4))
 
 #Write a Python function called rev_string() to reverse a string
-def rev_string()
+def rev_string(Alejandro):
+    return Alejandro [::-1]
+print (rev_string('Alejandro'))    
 
 #Write a Python function called num_within() to check whether a number falls in a given range.
+
 
 #Write a Python function called pascal() that prints out the first n rows of Pascal's triangle.
 
