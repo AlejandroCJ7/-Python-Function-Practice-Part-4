a733d5071bea7201efe207469086b79c5f1d1dbb
Alex Castaneda <alex@Alejandros-Mac.local>
2023-06-08 13:53:04 -0700

    solved multi_list function

--- PythonFunction_Part4.py
+++ PythonFunction_Part4.py
@@ -6,9 +6,12 @@ result = max_num(45, 68, 32)
 print(result) 
 
 #Write a Python function called mult_list() to multiply all the numbers in a list.
-
+def multi_list(a, b, c, d):
+    return a * b * c * d
+print (multi_list(2, 7, 8, 4))
 
 #Write a Python function called rev_string() to reverse a string
+def rev_string()
 
 #Write a Python function called num_within() to check whether a number falls in a given range.
 
